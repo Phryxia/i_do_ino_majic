fc37fc622a4d7ab5c33faf3e06852842ebec028b
Phryxia <xahhaepica@gmail.com>
2014-09-26 01:37:55 +0900

    20140926_Homework
    Do it your self

---
+++ src/hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	// Git Hub Tutorial!!!
+	printf("My name is Se Kyu Kwon\n");
+	
+	// Put your names to here
+	// prinf("My name is ...\n");
+	
+	// Don't forget to save & commit when you done!
+	
+	return 0;
+}
